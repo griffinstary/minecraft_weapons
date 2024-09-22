d7f2f81fe3fbde44410f15a6a85904714d7c4735
griffinstary <griffinstary@gmail.com>
2024-09-22 14:38:07 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+// Greeting
+let z = document.getElementById("greeting"); 
+
+function playGreeting() { 
+  z.play(); 
+  console.log("Play Greeting")
+} 
+
+// Sword SFX
+let a = document.getElementById("sword"); 
+
+function playSword() { 
+  a.play(); 
+  console.log("Play Sword")
+} 
+
+// Crossbox SFX
+let b = document.getElementById("crossbow"); 
+
+function playCrossbow() { 
+  b.play(); 
+  console.log("Play Crossbow")
+} 
+
+// GoldenSword SFX
+let c = document.getElementById("goldensword");
+
+function playGoldensword() {
+    c.play();
+    console.log("play golden sword")
+}
+
+// Firewhoosh SFX
+let d = document.getElementById("firewhoosh");
+
+function playFirewhoosh() {
+    d.play();
+    console.log("play Fire Whoosh")
+}
+
+// Lavabucket SFX
+let e = document.getElementById("lavabucket");
+
+function playLavabucket() {
+    e.play();
+    console.log("play lavabucket")
+}
+
+// Mace SFX
+let f = document.getElementById("mace");
+
+function playMace() {
+    f.play();
+    console.log("play mace")
+}
+
+// Pickaxe SFX
+let g = document.getElementById("pickaxe");
+
+function playPickaxe() {
+    g.play();
+    console.log("play pickaxe")
+}
+
+// Redsword SFX
+let h = document.getElementById("redsword");
+
+function playRedSword() {
+    h.play();
+    console.log("play redsword");
+}
+
+// Rubysword SFX
+let i = document.getElementById("rubysword");
+
+function playRubySword() {
+    i.play();
+    console.log("play rubysword");
+}
+
+// Sword 1 SFX
+let j = document.getElementById("greensword");
+
+function playGreenSword() {
+    j.play();
+    console.log("play greensword");    
+}
+    
+    
+
+// Pickaxe 2 SFX
+let k = document.getElementById("pickaxe2");
+
+function playPickaxe2() {
+    k.play();
+    console.log("play pickaxe2")
+}
+
+// Trident SFX
+let l = document.getElementById("trident");
+
+function playTrident() {
+    l.play();
+    console.log("play trident")
+}
+// Waterbucket SFX
+let m = document.getElementById("waterbucket");
+
+function playWaterbucket() {
+    m.play();
+    console.log("play waterbucket")
+}
